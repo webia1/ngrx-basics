1b942f7384a230969ffffff742d92ea45a8e5e63
Erkan Buelbuel <webia1.github@gmail.com>
2021-03-28 11:33:57 +0200

    counter is now old

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 // EBIA
 
 import { StoreModule } from '@ngrx/store';
-import { actionReducerMap } from './store/actionReducerMap';
+import { actionReducerMap } from './store/counter/actionReducerMap';
 import { CounterComponent } from './components/counter/counter.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { increment, multiply } from 'src/app/store/actions';
+import { increment, multiply } from 'src/app/store/counter/actions';
 import { State } from 'src/app/static/types';
 
 @Component({
